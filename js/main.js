8cd9abe252caec446e738179ae95d47087cc6171
Marcin Rudzinski <marcin.rudzinski@next-it.pl>
2020-06-14 23:24:35 +0200

    I add add class 'active' to the correct article

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const titleClickHandler = function (event) {
         activeLink.classList.remove('active');
     }
     /* add class 'active' to the clicked link */
+    event.preventDefault();
     const clickedElement = this;
     clickedElement.classList.add('active');
     /* remove class 'active' from all articles */
@@ -14,13 +15,14 @@ const titleClickHandler = function (event) {
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-
-
     /* get 'href' attribute from the clicked link */
-
+    const articleSelector = clickedElement.getAttribute('href');
     /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
